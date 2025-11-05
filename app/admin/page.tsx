e7215def5dac9ae484bafa362781bda66de8658e
siddhiGvc2 <sd@gvc.in>
2025-11-05 17:06:14 +0530

    Access Denied Removed

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -5,9 +5,9 @@ import { useAuth } from "../contexts/AuthContext";
 export default function AdminPage() {
   const { loggedInUser } = useAuth();
 
-  if (!loggedInUser || (loggedInUser.userType !== 'SUPERADMIN' && loggedInUser.userType !== 'ADMIN')) {
-    return <div className="flex items-center justify-center h-screen">Access Denied</div>;
-  }
+  // if (!loggedInUser || (loggedInUser.userType !== 'SUPERADMIN' && loggedInUser.userType !== 'ADMIN')) {
+  //   return <div className="flex items-center justify-center h-screen">Access Denied</div>;
+  // }
 
   return <AdminPanel />;
 }
